77cde42edbb4848dbfc17893fa80b001959f5db8
baihaqism <foxynr@gmail.com>
2024-05-28 22:41:02 +0700

    feat: Add role middleware for authorization
    - Added role middleware to validate user roles and permissions
    - Updated product controller to use role middleware for authorization
    - Updated product routes to include role-based access control
    refactor: Refactor user model
    
    - Updated user model to use Joi schema for validation
    - Removed redundant validation code from user model
    
    fix: Modify validation for product schema
    
    - Integrated Joi validation with Mongoose schema for product model
    - Updated product schema to enforce validation rules for name, category, description, price, and sellerId fields

--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -3,6 +3,8 @@ import { authenticateToken } from '../middleware/authMiddleware';
 import { getAllProducts, getProductById, createProduct, updateProduct, deleteProduct } from '../controllers/productController';
 import { productSchema } from '../utils/validation';
 import validate from '../middleware/validationMiddleware';
+import { checkSellerRole } from '../middleware/roleMiddleware'; // Use the correct path
+
 
 const router = express.Router();
 
@@ -10,8 +12,8 @@ router.use(authenticateToken);
 
 router.get('/', getAllProducts);
 router.get('/:id', getProductById);
-router.post('/', validate(productSchema), createProduct);
-router.put('/:id', validate(productSchema), updateProduct);
-router.delete('/:id', deleteProduct);
+router.post('/', validate(productSchema), checkSellerRole, createProduct);
+router.put('/:id', validate(productSchema), checkSellerRole, updateProduct);
+router.delete('/:id', checkSellerRole, deleteProduct);
 
 export default router;
