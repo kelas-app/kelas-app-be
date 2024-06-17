8d1398af24939c3d214b4d4c46304d6f0562cadb
dotnaonweh <hellonudes66@gmail.com>
2024-06-18 06:34:39 +0700

    feat: Add endpoint to update user profile
    - Added PUT endpoint /editprofile/:id to allow users to update their profiles
    - Implemented updateProfile controller function to handle profile updates
    - Implemented updateUserProfile service function to update user profile in the database
    - Updated userRoutes to include the new endpoint

--- src/middleware/validationMiddleware.ts
+++ src/middleware/validationMiddleware.ts
@@ -1,14 +1,14 @@
-import { Request, Response, NextFunction } from 'express';
-import { Schema } from 'joi';
+import { Request, Response, NextFunction } from "express"
+import { Schema } from "joi"
 
 const validate = (schema: Schema) => {
   return (req: Request, res: Response, next: NextFunction) => {
-    const { error } = schema.validate(req.body);
+    const { error } = schema.validate(req.body)
     if (error) {
-      return res.status(400).json({ message: error.details[0].message });
+      return res.status(400).json({ message: error.details[0].message })
     }
-    next();
-  };
-};
+    next()
+  }
+}
 
-export default validate;
+export default validate
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,28 +1,32 @@
-import express from 'express';
-import { authenticateToken } from '../middleware/authMiddleware';
+import express from "express"
 import {
-    createUser,
-    getUsers,
-    getUserById,
-    updateUser,
-    deleteUser,
-    addRatingAndReview,
-    getRatingsAndReviews,
-    downloadAllUsers
-} from '../controllers/userController';
+  createUser,
+  getUsers,
+  getUserById,
+  updateUser,
+  deleteUser,
+  addRatingAndReview,
+  getRatingsAndReviews,
+  downloadAllUsers,
+  updateProfile,
+} from "../controllers/userController"
+import { authenticateToken } from "../middleware/authMiddleware"
+import validate from "../middleware/validationMiddleware"
+import { updateUserProfileSchema } from "../utils/validation"
 
-const router = express.Router();
+const router = express.Router()
 
-router.use(authenticateToken);
+router.use(authenticateToken)
 
-router.get('/download', downloadAllUsers);
+router.get("/download", downloadAllUsers)
 
-router.post('/', createUser);
-router.get('/', getUsers);
-router.get('/:id', getUserById);
-router.put('/:id', updateUser);
-router.delete('/:id', deleteUser);
-router.post('/:id/ratings', addRatingAndReview);
-router.get('/:id/ratings', getRatingsAndReviews);
+router.post("/", createUser)
+router.get("/", getUsers)
+router.get("/:id", getUserById)
+router.put("/:id", updateUser)
+router.delete("/:id", deleteUser)
+router.post("/:id/ratings", addRatingAndReview)
+router.get("/:id/ratings", getRatingsAndReviews)
+router.put("/editprofile/:id", validate(updateUserProfileSchema), updateProfile)
 
-export default router;
+export default router
