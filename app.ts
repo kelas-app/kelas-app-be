376d332cab443393f1cac8650acc13ae23af4086
dotnaonweh <hellonudes66@gmail.com>
2024-06-21 00:19:04 +0700

    feat: Add Swagger UI and API documentation route
    - Updated server.ts to handle /api-docs route
    - Created public/swagger.html for Swagger UI
    - Moved apidocs.json to docs directory

--- app.ts
+++ app.ts
@@ -1,56 +1,49 @@
-import express from "express";
-import mongoose from "mongoose";
-import cors from "cors";
-import morgan from "morgan";
-import moment from "moment";
-import authRoutes from "./src/routes/authRoutes";
-import userRoutes from "./src/routes/userRoutes";
-import productRoutes from "./src/routes/productRoutes";
-import orderRoutes from "./src/routes/orderRoutes";
-import cartRoutes from "./src/routes/cartRoutes";
-import conversationRoutes from "./src/routes/conversationRoutes";
-import messageRoutes from "./src/routes/messageRoutes";
-import wishlistRoutes from "./src/routes/wishlistRoutes";
-import interactionRoutes from './src/routes/interactionRoutes';
-import { errorHandler } from "./src/middleware/errorMiddleware";
-import path from "path";
+import express from "express"
+import mongoose from "mongoose"
+import cors from "cors"
+import morgan from "morgan"
+import { errorHandler } from "./src/middleware/errorMiddleware"
+import authRoutes from "./src/routes/authRoutes"
+import userRoutes from "./src/routes/userRoutes"
+import productRoutes from "./src/routes/productRoutes"
+import orderRoutes from "./src/routes/orderRoutes"
+import cartRoutes from "./src/routes/cartRoutes"
+import conversationRoutes from "./src/routes/conversationRoutes"
+import messageRoutes from "./src/routes/messageRoutes"
+import wishlistRoutes from "./src/routes/wishlistRoutes"
+import interactionRoutes from "./src/routes/interactionRoutes"
+import path from "path"
+import moment from "moment"
 
-const app = express();
+const app = express()
 
-morgan.token("date", (req, res) => moment().format());
+morgan.token("date", (req, res) => moment().format())
 morgan.token("message", (req, res) =>
   res.statusCode < 400 ? "success" : "error"
-);
+)
 
 const customMorganFormat =
-  "[:date[iso]] :method :url :status :response-time ms - :res[content-length] :message";
+  "[:date[iso]] :method :url :status :response-time ms - :res[content-length] :message"
 
-app.use(cors());
-app.use(express.json());
-app.use(morgan(customMorganFormat));
-
+app.use(cors())
+app.use(express.json())
+app.use(morgan(customMorganFormat))
 
 mongoose
   .connect(process.env.MONGODB_URI as string)
   .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.error("MongoDB connection error:", err));
+  .catch((err) => console.error("MongoDB connection error:", err))
 
-app.use("/api/auth", authRoutes);
-app.use("/api/users", userRoutes);
-app.use("/api/products", productRoutes);
-app.use("/api/orders", orderRoutes);
-app.use("/api/cart", cartRoutes);
-app.use("/api/conversations", conversationRoutes);
-app.use("/api/messages", messageRoutes);
-app.use("/api/wishlist", wishlistRoutes);
-app.use('/api/interactions', interactionRoutes);
+app.use("/api/auth", authRoutes)
+app.use("/api/users", userRoutes)
+app.use("/api/products", productRoutes)
+app.use("/api/orders", orderRoutes)
+app.use("/api/cart", cartRoutes)
+app.use("/api/conversations", conversationRoutes)
+app.use("/api/messages", messageRoutes)
+app.use("/api/wishlist", wishlistRoutes)
+app.use("/api/interactions", interactionRoutes)
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(errorHandler)
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
-
-app.use(errorHandler);
-
-export default app;
+export default app
--- server.ts
+++ server.ts
@@ -1,8 +1,28 @@
-import app from "./app";
-import config from "./config/config";
+import express from "express"
+import path from "path"
+import config from "./config/config"
+import app from "./app"
+
+const PORT = config.port || 3000
+const server = express()
+
+server.use(express.static(path.join(__dirname, "public")))
+
+server.get("/api-docs", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "swagger.html"))
+})
+
+server.get("/api-docs/apidocs.json", (req, res) => {
+  res.sendFile(path.join(__dirname, "docs", "apidocs.json"))
+})
+
+server.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"))
+})
+
+server.use(app)
 
 // Start the server
-const PORT = config.port || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
